60955724cbf6fc7f54d7e1d79cb683fc026db5f2
dmilk <dmitry_kul@mail.ru>
2014-03-28 16:29:46 +0400

    Initial commit

---
+++ src/main/java/first/Hello.java
@@ -0,0 +1,27 @@
+package first;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by 1 on 27.03.2014.
+ */
+@Path("/hello")
+public class Hello {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayPlainTextHello(){
+        return "XxxxxxxxX";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public String sayXMLHello(){
+        return "<?xml version=\"1.0\"?>"+"<hello>Готово xml</hello>";
+    }
+
+
+}
